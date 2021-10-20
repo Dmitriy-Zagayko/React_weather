4645b664d7a42ffb97ca5bab0d9b1cb79761bf1d
Dmitriy <akselmega95@gmail.com>
2021-10-20 19:09:21 +0300

    Create Input and add propTypes

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './button.scss';
 
 const Button = ({ title, icon, onClick }) => {
@@ -9,7 +10,13 @@ const Button = ({ title, icon, onClick }) => {
 		>
 			{title}
 		</button>
-	)
-}
+	);
+};
 
 export default Button;
+
+Button.propTypes = {
+	icon: PropTypes.string,
+	title: PropTypes.string,
+	onClick: PropTypes.func,
+};
