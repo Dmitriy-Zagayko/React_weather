64c4ae25ace00276b3cacdef69552d2f1f5f7cdd
Dmitriy <akselmega95@gmail.com>
2021-10-20 16:16:38 +0300

    Create Button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import logo from '../../../public/assets/images/logo.png';
 
 const Header = () => (
 	<header>
-		<img src={logo}/>
+		<img src={logo} />
 		<h1>Weather forecast</h1>
 	</header>
-)
+);
 
 export default Header;
