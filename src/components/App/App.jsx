bc12bd1dc73a65448a054a66b4fab8d4703efc06
Dmitriy <akselmega95@gmail.com>
2021-10-20 15:01:40 +0300

    Start setting for app Weather

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.scss';
+
+
+const App = () => (
+	<>
+		<div>Start successfully</div>
+	</>
+)
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const pages = [
+	{
+		template: path.resolve(__dirname, 'public/index.html'),
+		filename: 'index.html',
+	},
+];
+const getFileLoader = (regExp) => ({
+	test: regExp,
+	use: ['file-loader'],
+});
+const getStyleLoader = (regExp, additionalLoaders) => {
+	const rules = {
+		test: regExp,
+		use: ['style-loader', 'css-loader'],
+	};
+	if (additionalLoaders && additionalLoaders.length) {
+		additionalLoaders.forEach((loader) => rules.use.push(loader));
+	}
+	return rules;
+};
+
+module.exports = {
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		main: ['@babel/polyfill', './index.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[contenthash].js',
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+		},
+	},
+	devServer: {
+		port: 5050,
+	},
+	plugins: [
+		...pages.map((config) => new HTMLWebpackPlugin(config)),
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, 'public/assets'),
+					to: path.resolve(__dirname, 'dist/assets'),
+				},
+			],
+		}),
+	],
+	module: {
+		rules: [
+			getStyleLoader(/\.css$/),
+			getFileLoader(/\.(ttf|woff|eot)$/),
+			getFileLoader(/\.(jpe?g|svg|png)$/),
+			getStyleLoader(/\.s[ac]ss$/, ['sass-loader']),
+			{
+				test: /\.(jsx|js)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react', '@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+};
